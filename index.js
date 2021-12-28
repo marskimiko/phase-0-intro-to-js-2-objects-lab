52ed5e14c776714ea07f00d73c9502153373e5df
marisasaunders <marisaksaunders@gmail.com>
2021-12-27 22:17:20 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
+//const { validateUseBuiltInsOption } = require("@babel/preset-env/lib/normalize-options")
+
 // Write your solution in this file!
+
+const employee = {
+    name: " ",
+    streetAddress: " ",
+}
+
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newEmployee = { ...employee };
+    
+    newEmployee[key] = value;
+
+    return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const newEmployee = {...employee};
+    
+    delete newEmployee[key];
+    
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key]
+
+    return employee;
+
+}
